7ce33ebbecaf81a58da7cf25fba69dc93e9cc49f
mfurkansahn <76617516+mfurkansahn@users.noreply.github.com>
2024-03-26 22:07:57 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { FormsModule } from '@angular/forms';
+import { DeletedTodoComponent } from './components/deleted-todo/deleted-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    DeletedTodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule, //projeye import edince kütüphane çalışır
+    FormsModule //addTodo hmtl içindeki submit butonu için ilk olarak eklemem lazım
   ],
   providers: [],
   bootstrap: [AppComponent]
